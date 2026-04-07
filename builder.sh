82f8601bff93271dc8f7d78920dc3c8144a204c3
mlauer30 <mlauer07@gmail.com/>
2026-04-07 14:59:18 -0400

    message bar is rose-pine themed

--- builder.sh
+++ builder.sh
@@ -3,7 +3,6 @@
 ln -s ~/.config/tmux/tmux.conf ~/.tmux.conf 
 
 # manual install of plugins tmux-continuum and tmux-resurrect
-
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # manual install of tmux-resurrect
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
